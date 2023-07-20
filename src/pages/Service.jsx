95742645f3dd5b460e917c2362da2b8bd9d87b8d
JustyTry <amaslak13@gmail.com>
2023-07-20 23:13:18 +0700

    Service page

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -3,8 +3,13 @@ import services from "../services.json";
 const Service = () => {
     console.log(services)
     return (
-        <div>
-            <div></div>
+        <div className="w-full bg-light">
+            <div>
+                <h1>Title</h1>
+            </div>
+            <div>
+                desc
+            </div>
         </div>
     )
 }

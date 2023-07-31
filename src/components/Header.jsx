f6a613aa3d21ef74798fdbef334d6c65e0ea7cf5
JustyTry <amaslak13@gmail.com>
2023-07-31 23:32:01 +0700

    Service texts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,16 @@ import Logo from '/images/logo.png'
 import { RxHamburgerMenu } from 'react-icons/rx'
 import { Link } from 'react-router-dom'
 
-
 const Header = () => {
     return (
-        <header className="w-full text-darkblue flex justify-center bg-light">
+        <header className="w-full text-white flex justify-center bg-light">
             <div className='w-8/12 max-lg:w-full absolute h-20 flex justify-around items-center max-md:justify-between '>
                 <Link to="/" className='h-full w-3/12 max-md:w-8/12'><img className='h-full py-3 max-md:ml-4' src={Logo} alt="Logo" /></Link>
                 <nav className='max-md:hidden w-6/12 flex justify-center'>
                     <ul className="text-xl flex gap-10">
-                        <li><Link to="/about">Кто мы</Link></li>
-                        <li><Link to="/services">Услуги</Link></li>
-                        <li><Link to="/contacts">Контакты</Link></li>
+                        <li className='before:absolute before:bg-white before:bottom-6 before:h-px before:w-0 hover:before:w- before:duration-200'><Link to="/about">Кто мы</Link></li>
+                        <li className='before:absolute before:bg-white before:bottom-6 before:h-px before:w-0 hover:before:w-16 before:duration-200'><Link to="/services" >Услуги</Link></li>
+                        <li className='before:absolute before:bg-white before:bottom-6 before:h-px before:w-0 hover:before:w-16 before:duration-200'><Link to="/contacts">Контакты</Link></li>
                     </ul>
                 </nav>
                 <div className='max-md:hidden w-3/12 text-right text-xl'>
@@ -23,8 +22,8 @@ const Header = () => {
                 </div>
 
             </div>
-            <div className='w-full'>
-                <img src="https://images.unsplash.com/photo-1437751068958-82e6fccc9360?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="" className='w-full select-none pointer-events-none opacity-0' />
+            <div className='w-full bg-darkblue h-[48rem]'>
+                {/* <img src="https://thumbs.dreamstime.com/z/gavel-scales-justice-law-books-judge-court-66558437.jpg?w=992" alt="" className='w-full max-h-[48rem] select-none pointer-events-none ' /> */}
             </div>
         </header>
     )
--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
 
 const Record = (data) => {
+
     return (
         <div className="w-30% h-64 max-md:w-2/5 max-sm:w-full overflow-hidden ">
-            <Link to={`service/${data.link}`}>
+            <Link to={`/service/${data.link}`}>
                 <img
                     src={data.image}
                     alt={data.title}
@@ -13,8 +14,8 @@ const Record = (data) => {
                     width={256}
                 />
 
-                <div className="capitalize bg-beige w-full text-white text-lg pl-6 py-2 flex justify-between h-2/12 object-cover whitespace-nowrap relative overflow-hidden">
-                    <p className="">{data.title}</p> <span className="pr-3 font-bold text-xl absolute right-0 bg-beige">→</span>
+                <div className=" bg-beige w-full text-white text-lg pl-6 py-2 flex justify-between h-2/12 object-cover whitespace-nowrap relative overflow-hidden">
+                    <p className=""><span className="capitalize">{data.title[0]}</span>{data.title.slice(1)}</p> <span className="pr-3 font-bold text-xl absolute right-0 bg-beige">→</span>
                 </div>
             </Link>
         </div>
